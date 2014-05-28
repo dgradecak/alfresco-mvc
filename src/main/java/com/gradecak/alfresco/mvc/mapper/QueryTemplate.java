8c9715d0dd750ee09c609d4d7b3f14e6d4eda304
Daniel Gradecak <daniel@gradecak.com>
2014-05-28 13:51:43 +0200

    incorrect result size exception + localized message

--- src/main/java/com/gradecak/alfresco/mvc/mapper/QueryTemplate.java
+++ src/main/java/com/gradecak/alfresco/mvc/mapper/QueryTemplate.java
@@ -21,85 +21,102 @@ import com.gradecak.alfresco.mvc.Query;
 
 public class QueryTemplate {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(QueryTemplate.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(QueryTemplate.class);
 
-	private int defaultMaxItems = 100;
-	private ServiceRegistry serviceRegistry;
-	
-	public QueryTemplate() {
+  private int defaultMaxItems = 100;
+  private ServiceRegistry serviceRegistry;
+
+  public QueryTemplate() {}
+
+  public QueryTemplate(final ServiceRegistry serviceRegistry) {
+    this.serviceRegistry = serviceRegistry;
   }
 
-	public QueryTemplate(final ServiceRegistry serviceRegistry) {
-		this.serviceRegistry = serviceRegistry;
-	}
-
-	public QueryTemplate(final ServiceRegistry serviceRegistry, final int maxItems) {
-		this.serviceRegistry = serviceRegistry;
-		this.defaultMaxItems = maxItems;
-	}
-
-	public <T> T queryForObject(final NodeRef nodeRef, final NodeMapper<T> mapper) {
-		Map<QName, Serializable> properties = serviceRegistry.getNodeService().getProperties(nodeRef);
-		return mapper.mapNode(properties);
-	}
-
-	public <T> T queryForObject(final Query query, final NodeMapper<T> mapper) {
-		return queryForObject(query.toString(), mapper);
-	}
-
-	public <T> T queryForObject(final String query, final NodeMapper<T> mapper) {
-		List<T> list = queryForList(query.toString(), mapper, defaultMaxItems, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, SearchService.LANGUAGE_LUCENE);
-		if (list.size() == 1) {
-			return list.get(0);
-		} else {
-			LOGGER.error("One result was expected, found " + list.size());
-			throw new RuntimeException("One result was expected, found more.");
-		}
-	}
-
-	public <T> List<T> queryForList(final Query query, final NodeMapper<T> mapper) {
-		return queryForList(query.toString(), mapper, defaultMaxItems, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, SearchService.LANGUAGE_LUCENE);
-	}
-
-	public <T> List<T> queryForList(final String query, final NodeMapper<T> mapper, final int maxItems, final StoreRef store, final String searchLanguage) {
-		Assert.notNull(query);
-		Assert.notNull(mapper);
-		Assert.isTrue(maxItems > 0, "maxItems must be a positive integer");
-		Assert.notNull(store);
-		Assert.hasText(searchLanguage);
-
-		List<T> list = new ArrayList<T>();
-
-		SearchParameters sp = new SearchParameters();
-		sp.addStore(store);
-		sp.setLanguage(searchLanguage);
-		sp.setMaxItems(maxItems);
-		sp.setQuery(query.toString());
-		ResultSet results = null;
-		try {
-			results = serviceRegistry.getSearchService().query(sp);
-			if (results != null && results.length() != 0) {
-				for (ResultSetRow resultSetRow : results) {
-					NodeRef nodeRef = resultSetRow.getNodeRef();
-					if (serviceRegistry.getNodeService().exists(nodeRef)) {
-						list.add(queryForObject(nodeRef, mapper));
-					}
-				}
-			}
-		} finally {
-			if (results != null) {
-				results.close();
-			}
-		}
-
-		return list;
-	}
-	
-	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-	  this.serviceRegistry = serviceRegistry;
+  public QueryTemplate(final ServiceRegistry serviceRegistry, final int maxItems) {
+    this.serviceRegistry = serviceRegistry;
+    this.defaultMaxItems = maxItems;
   }
-	
-	public void setDefaultMaxItems(int maxItems) {
-	  this.defaultMaxItems = maxItems;
+
+  public <T> T queryForObject(final NodeRef nodeRef, final NodeMapper<T> mapper) {
+    Assert.notNull(mapper);
+    Assert.notNull(nodeRef);
+
+    Map<QName, Serializable> properties = serviceRegistry.getNodeService().getProperties(nodeRef);
+    return mapper.mapNode(properties);
+  }
+
+  public <T> T queryForObject(final Query query, final NodeMapper<T> mapper) {
+    return queryForObject(query.toString(), mapper);
+  }
+
+  public <T> T queryForObject(final String query, final NodeMapper<T> mapper) throws IncorrectResultSizeException {
+    Assert.notNull(mapper);
+    Assert.hasText(query);
+
+    List<T> list = queryForList(query.toString(), mapper, defaultMaxItems, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,
+        SearchService.LANGUAGE_LUCENE);
+    int size = list.size();
+    switch (size) {
+    case 0:
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("No result found. Returning null.");
+      }
+      return null;
+    case 1:
+      return list.get(0);
+    default:
+      LOGGER.error(IncorrectResultSizeException.DEFAULT_MESSAGE_ID, size);
+      throw new IncorrectResultSizeException(size);
+    }
+  }
+
+  public <T> List<T> queryForList(final Query query, final NodeMapper<T> mapper) {
+    return queryForList(query.toString(), mapper, defaultMaxItems, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, SearchService.LANGUAGE_LUCENE);
+  }
+
+  public <T> List<T> queryForList(final String query, final NodeMapper<T> mapper, final int maxItems, final StoreRef store,
+      final String searchLanguage) {
+    Assert.notNull(query);
+    Assert.notNull(mapper);
+    Assert.isTrue(maxItems > 0, "maxItems must be a positive integer");
+    Assert.notNull(store);
+    Assert.hasText(searchLanguage);
+
+    List<T> list = new ArrayList<T>();
+
+    SearchParameters sp = new SearchParameters();
+    sp.addStore(store);
+    sp.setLanguage(searchLanguage);
+    sp.setMaxItems(maxItems);
+    sp.setQuery(query.toString());
+    ResultSet results = null;
+    try {
+      results = serviceRegistry.getSearchService().query(sp);
+      if (results != null && results.length() != 0) {
+        for (ResultSetRow resultSetRow : results) {
+          NodeRef nodeRef = resultSetRow.getNodeRef();
+          if (serviceRegistry.getNodeService().exists(nodeRef)) {
+            if (LOGGER.isDebugEnabled()) {
+              LOGGER.debug("Adding a row to the result list. NodeRef: " + nodeRef);
+            }
+            list.add(queryForObject(nodeRef, mapper));
+          }
+        }
+      }
+    } finally {
+      if (results != null) {
+        results.close();
+      }
+    }
+
+    return list;
+  }
+
+  public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+    this.serviceRegistry = serviceRegistry;
+  }
+
+  public void setDefaultMaxItems(int maxItems) {
+    this.defaultMaxItems = maxItems;
   }
 }
