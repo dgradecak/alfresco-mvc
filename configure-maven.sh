62bc5820e3dd8dc089a57d07841f6e6db31c5d48
Daniel Gradecak <daniel@gradecak.com>
2017-10-23 14:44:39 +0200

    Gradecak-Repo

--- configure-maven.sh
+++ configure-maven.sh
@@ -2,7 +2,7 @@
 
 sed -i~ "/<servers>/ a\
 <server>\
-  <id>private-repo</id>\
+  <id>Gradecak-Repo</id>\
   <username>${GRA_USERNAME}</username>\
   <password>${GRA_PASS}</password>\
 </server>" /usr/share/maven/conf/settings.xml
