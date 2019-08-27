fc10f8c9cc345feea68d829cf1f2b70e53e5dc33
Daniel Gradecak <daniel@gradecak.com>
2019-08-27 19:19:40 +0200

    preparing release 7.0.0

--- configure-maven.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i~ "/<servers>/ a\
-<server>\
-  <id>Gradecak-Repo</id>\
-  <username>${GRA_USERNAME}</username>\
-  <password>${GRA_PASS}</password>\
-</server>" /usr/share/maven/conf/settings.xml
